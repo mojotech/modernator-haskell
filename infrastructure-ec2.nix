23a4a1d20c97f86f9f5524887f0fb756398411b1
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-01-13 16:49:50 -0500

    Pull out aws config vars

--- infrastructure-ec2.nix
+++ infrastructure-ec2.nix
@@ -1,20 +1,21 @@
 let
-  # Insert your AWS access key here
-  accessKey = "personal";
-in {
+  config = import ./ec2-config.nix;
+in with config; {
   modernator-haskell = { resources, ... }:
     { deployment.targetEnv = "ec2";
       deployment.ec2.region = "us-east-1";
       deployment.ec2.instanceType = "t2.nano";
-      deployment.ec2.accessKeyId = accessKey;
+      deployment.ec2.accessKeyId = awsProfile;
       # We'll let NixOps generate a keypair automatically
       deployment.ec2.keyPair = resources.ec2KeyPairs.modernator-haskell-kp.name;
-      deployment.ec2.securityGroups = [ "ssh" "HTTP/HTTPS" ];
+      deployment.ec2.securityGroupIds = securityGroupIds;
+      deployment.ec2.subnetId = subnet;
+      deployment.ec2.associatePublicIpAddress = true;
     };
 
   # Here we create a keypair in the same region as our deployment
   resources.ec2KeyPairs.modernator-haskell-kp = {
     region = "us-east-1";
-    accessKeyId = accessKey;
+    accessKeyId = awsProfile;
   };
 }
