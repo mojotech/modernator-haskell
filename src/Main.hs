a5258b316e904699a45528b9428f065f888a91bd
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-01-13 16:49:24 -0500

    Add envvar for extra cors origins

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ import Data.Maybe (fromMaybe, mapMaybe)
 import Data.List (intersperse)
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.Cors (cors, simpleCorsResourcePolicy, corsOrigins, corsRequestHeaders)
+import qualified Data.ByteString.Char8 as BS
 
 prettyArg s = fmap ((s ++) . show)
 
@@ -16,16 +17,17 @@ main = do
     port <- fmap (fromMaybe 8080 . (>>= readMaybe)) $ lookupEnv "MODERNATOR_PORT"
     stateDir <- lookupEnv "MODERNATOR_STATE_DIR"
     keyDir <- fmap (fromMaybe "./") $ lookupEnv "MODERNATOR_KEY_DIR"
+    extraCors <- fmap (map BS.pack . words . fromMaybe "") $ lookupEnv "MODERNATOR_CORS"
     let args = mapMaybe id [ prettyArg "port: " (pure port)
                            , prettyArg "state directory: " stateDir
                            ]
     putStrLn $ "Running modernator-haskell on " ++ (concat $ intersperse " and " args)
     app <- mkApp stateDir keyDir
-    run port $ logStdoutDev $ myCors app
+    run port $ logStdoutDev $ myCors extraCors app
 
-myCors = cors (const $ Just policy)
+myCors extra = cors (const $ Just (policy extra))
 
-policy = simpleCorsResourcePolicy
-    { corsOrigins = Just (["http://localhost:3000", "http://localhost:8080"], True)
+policy extra = simpleCorsResourcePolicy
+    { corsOrigins = Just ([ "http://localhost:8080" ] ++ extra, True)
     , corsRequestHeaders = ["Content-Type"]
     }
